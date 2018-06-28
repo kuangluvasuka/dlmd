3d173a17df0db420141d747202d9a422d9f3e3d8
Yui Gakki <lucifernine86@gmail.com>
2018-06-27 22:05:31 -0700

    add src folder, containing model files

---
+++ src/mpnn.py
@@ -0,0 +1,36 @@
+"""
+Message Passing Neural Network (MPNN) for quantum molecular
+simulation training.
+
+"""
+
+import tensorflow as tf
+
+from utils.logger import log
+
+
+class Model(object):
+  """Super class for models.
+  """
+  def __init__(self):
+    pass
+
+
+
+
+class MPNN(Model):
+  """
+  """
+  
+  def __init__(self):
+    #super
+    pass
+
+
+
+
+
+
+
+
+
