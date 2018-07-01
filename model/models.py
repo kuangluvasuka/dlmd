15272cf6c011e032a6acb525026d0c1899196dc7
Yui Gakki <lucifernine86@gmail.com>
2018-06-30 21:39:54 -0700

    added message function class, need to complete mpnn matrix transformation

--- model/models.py
+++ model/models.py
@@ -17,13 +17,37 @@ class Model(object):
     """
 
     self.params = params
+    self.function = None
   
   def _fprop(self, *args, **kwargs):
     raise NotImplementedError("Subclass must define _fprop() method")
 
 
 class MessageFunction(Model):
-  pass
+  """Message function class"""
+
+  def __init__(self, params):
+    super(MessageFunction).__init__(params)
+    self._select_function(self.params.message_function)
+
+  def _select_function(self, key, *args):
+    
+    self.function = {
+      'mpnn': self._mpnn
+
+    }.get(key)
+
+  def _mpnn(self, node_state, adj_mat):
+    #TODO: Need to figure out the matrix operations 
+    #tf.matmul(node_state, adj_mat)
+
+
+  def _fprop(self):
+    return self._mpnn(node_state, adj_mat)
+
+
+
+
 
 
 class UpdateFunction(Model):
