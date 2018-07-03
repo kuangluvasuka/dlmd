5e66b5781fa80da0087026c1ec1ad2e3cc3928af
Yui Gakki <lucifernine86@gmail.com>
2018-07-02 23:45:21 -0700

    add training framework

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import numpy as np
 
 # Local modules
 from utils.logger import log
+from model.mpnn import MPNN
 #from dataset import xyz_parser
 
 # parse arguments
@@ -26,29 +27,58 @@ def main():
 
   datapath = args.datapath
 
+  # Create data loading stream
   dataset = tf.data.TFRecordDataset('./dataset/qm9.tfrecords')
   dataset = dataset.map(_parse_function)
   dataset = dataset.batch(1)
   iterator = dataset.make_initializable_iterator()
   next_elet = iterator.get_next()
 
+
+  # Create model for training
+  hparams = MPNN.default_hparams()
+  model = MPNN(hparams)
+
+
+
   with tf.Session() as sess:
 
     sess.run(iterator.initializer)
     g, h, e, l = sess.run(next_elet)
-    #log.infov(e)
+
+
+    # TODO: need to populate this framework
+    for step in range(1, N_step):
+      try:
+        sess.run(train_op)
+      except tf.error.OutOfRangeError:
+        # Reload the iterator when it reaches the end of the dataset
+        sess.run(iterator.initializer, 
+                 feed_dict={_data: #dataset
+                            _label: #label
+                 })
+        sess.run(train_op)
+      
+      if step % display_step == 0 or step == 1:
+
+        loss, acc = sess.run([loss_op, accuracy])
+        log.info()
+
+
+
+
+
 
 def _parse_function(record):
   features = {'l': tf.FixedLenFeature((), dtype=tf.string, default_value=""),
               'g': tf.FixedLenFeature((), dtype=tf.string, default_value=""),
               'h': tf.FixedLenFeature((), dtype=tf.string, default_value=""),
               'e': tf.FixedLenFeature((), dtype=tf.string, default_value="")}
-
   parsed = tf.parse_single_example(record, features)
   l = tf.decode_raw(parsed['l'], tf.float64)
   g = tf.decode_raw(parsed['g'], tf.float64)
   h = tf.decode_raw(parsed['h'], tf.float64)
-  #e = tf.decode_raw(parsed['e'].values, tf.float32)
+  e = tf.decode_raw(parsed['e'], tf.float64)
 
   # TODO: add zero padding for data batching
 
