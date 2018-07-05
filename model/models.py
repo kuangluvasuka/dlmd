453b8c39e69b76deaca745792befb33bf5343a35
Yui Gakki <lucifernine86@gmail.com>
2018-07-05 14:16:06 -0700

    daily updating

--- model/models.py
+++ model/models.py
@@ -17,10 +17,13 @@ class Model(object):
     """
 
     self.params = params
-    self.function = None
+    self._init_graph()
   
   def _fprop(self, *args, **kwargs):
-    raise NotImplementedError("Subclass must define _fprop() method")
+    raise NotImplementedError("Subclass must define _fprop() method.")
+
+  def _init_graph(self):
+    raise NotImplementedError("Subclass must define _init_graph() method.")
 
 
 class MessageFunction(Model):
@@ -29,21 +32,46 @@ class MessageFunction(Model):
   def __init__(self, params):
     super(MessageFunction).__init__(params)
     self._select_function(self.params.message_function)
+  
+  def _init_graph(self):
+    
+
 
   def _select_function(self, key, *args):
     
-    self.function = {
+    self._function = {
       'mpnn': self._mpnn
 
     }.get(key)
 
-  def _mpnn(self, node_state, adj_mat):
+  def _fprop(
+    self, 
+    node_state,
+    adj_mat,
+    reuse_graph_tensors=False):
+    """Compute a_v^t from h_v^{t-1}.
+    
+    Args:
+      node_state (tf.float32): [batch_size, num_node, node_dim]
+      adj_mat (tf.int32): [batch_size, node_dim, node_dim]
+      resule_graph_tensors (boolean):
+    """
+
+    #if not reuse_graph_tensors:
+    #  self._init_graph(adj_mat)
+    
+    #tf.Assert(self._a_in, adj_mat)
+
+    return self._function(node_state, adj_mat)
+
+  def _mpnn(self, node_state):
+    """ 
+    """
     #TODO: Need to figure out the matrix operations 
-    #tf.matmul(node_state, adj_mat)
+    a_in = tf.matmul(node_state, self._a_in)
+    a_out = tf.matmul(node_state, self._a_out)
 
 
-  def _fprop(self):
-    return self._mpnn(node_state, adj_mat)
 
 
 
--- model/mpnn.py
+++ model/mpnn.py
@@ -16,8 +16,14 @@ class MPNN(models.Model):
   def default_hparams():
     return tf.contrib.training.HParams(
       batch_size=1,
+      padded_input_dim=73,
+      hidden_node_dim=50,
+      hidden_edge_dim=50,
       epoch=10,
       #TODO: more hps
+      message_function='mpnn'
+      update_function=''
+      readout_function=''
 
     )
 
