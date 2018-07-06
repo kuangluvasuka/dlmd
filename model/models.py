2e458f89046b3bee2123397f80c44600d0d1fd53
Yui Gakki <lucifernine86@gmail.com>
2018-07-06 00:26:11 -0700

    add tf.matmul in _mpnn()

--- model/models.py
+++ model/models.py
@@ -34,10 +34,28 @@ class MessageFunction(Model):
     self._select_function(self.params.message_function)
   
   def _init_graph(self):
+    """Construct learnable variables matrix_in and matrix out.
     
+    This function is called in base Model's constructor, so every
+    MessageFunction object will generate it's own learnable variables
+    once instantiated.
+    """
 
+    self._matrix_in = tf.get_variable(
+      'matrix_weights_in'
+      shape=[self.params.num_edge_class, self.params.node_dim, self.params.node_dim])
+    self._matrix_out = tf.get_variable(
+      'matrix_weight_out',
+      shape=[self.params.num_edge_class, self.params.node_dim, self.params.node_dim])
+    #tf.Print(self._matrix_out, [self._matrix_out])
+
+    #TODO: Add variables for Non-bounding connection
+    if params.non_edge:
+      pass
 
   def _select_function(self, key, *args):
+    """
+    """
     
     self._function = {
       'mpnn': self._mpnn
@@ -52,8 +70,8 @@ class MessageFunction(Model):
     """Compute a_v^t from h_v^{t-1}.
     
     Args:
-      node_state (tf.float32): [batch_size, num_node, node_dim]
-      adj_mat (tf.int32): [batch_size, node_dim, node_dim]
+      node_state (tf.float32): [batch_size, num_nodes, node_dim]
+      adj_mat (tf.int32): [batch_size, num_nodes, num_nodes]
       resule_graph_tensors (boolean):
     """
 
@@ -64,13 +82,21 @@ class MessageFunction(Model):
 
     return self._function(node_state, adj_mat)
 
-  def _mpnn(self, node_state):
+  def _mpnn(self, node_state, adj_mat):
     """ 
     """
-    #TODO: Need to figure out the matrix operations 
-    a_in = tf.matmul(node_state, self._a_in)
-    a_out = tf.matmul(node_state, self._a_out)
 
+    #TODO: Need to figure out the matrix operations 
+    a_in = tf.gather(self._matrix_in, adj_mat)
+    a_out = tf.gather(self._matrix_out, tf.transpose(adj_mat, [0, 2, 1]))
+    #tf.Print()
+    #tf.Assert()
+
+    a_in = tf.matmul(node_state, a_in)
+    a_out = tf.matmul(node_state, a_out)
+    #tf.Assert()
+
+    #a_in = tf.transpose
 
 
 
