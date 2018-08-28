e1b60258e4b4c2b163b629d4698bd41459b8b742
CACS-SONY\lucif <lucifernine86@gmail.com>
2018-08-28 16:26:03 -0700

    extract node and edge features

--- dataset/mols_xyz_parser.py
+++ dataset/mols_xyz_parser.py
@@ -18,23 +18,18 @@ def xyz_graph_decoder(xyzfile):
 
     # Atom properties
     atom_property = []
-
-
-#TODO: create a hash that maps str index (a_prop[-1]) to ordered int
-
-
-    adj_list = []
+    index_hash = {}
     for i in range(na):
       a_prop = f.readline()
       a_prop = a_prop.replace('.*^', 'e')
       a_prop = a_prop.replace('*^', 'e')
       a_prop = a_prop.split()
       atom_property.append(a_prop[:-1])
-      adj_list.append(int(a_prop[-1]))
+      index_hash[a_prop[-1]] = i
 
     # Add nodes
     for i in range(na):
-      g.add_node(adj_list[i],
+      g.add_node(i,
                  a_symbol=atom_property[i][0],
                  coord=np.array(atom_property[i][1:4]).astype(np.float),
                  pc=float(atom_property[i][4]))
@@ -46,19 +41,21 @@ def xyz_graph_decoder(xyzfile):
       e_prop = e_prop.replace('*^', 'e')
       e_prop = e_prop.split()
 
-      atom_i = int(e_prop[1])
+      atom_i = index_hash[e_prop[1]]
       num_neighbor = int(e_prop[2])
       for j in range(num_neighbor):
-        atom_j = int(e_prop[2*j+3])
-        if atom_j in adj_list:
+        try:
+          atom_j = index_hash[e_prop[2*j+3]]
           g.add_edge(atom_i, atom_j,
             bo=float(e_prop[2*j+4]),
             distance=np.linalg.norm(g.node[atom_i]['coord'] - g.node[atom_j]['coord']))
+        except KeyError:
+          continue
     
     h = _mol_nodes(g)
     g, e = _mol_edges(g)
-    
-    return g, h, e, l
+
+    return g, h, e, label
 
 
 def _mol_nodes(g):
@@ -80,13 +77,13 @@ def _mol_edges(g):
 
     edge[(n1, n2)] = e_t
 
-    g = nx.to_numpy_matrix(g)
-    e = np.zeros((g.shape[0], g.shape[1], len(list(edge.values())[0])))
-    for edg in edge.keys():
-      e[edg[0], edg[1], :] = edge[edg]
-      e[edg[1], edg[0], :] = edge[edg]
+  g = nx.to_numpy_matrix(g)
+  e = np.zeros((g.shape[0], g.shape[1], len(list(edge.values())[0])))
+  for edg in edge.keys():
+    e[edg[0], edg[1], :] = edge[edg]
+    e[edg[1], edg[0], :] = edge[edg]
 
-    return g, e
+  return g, e
 
 
 
