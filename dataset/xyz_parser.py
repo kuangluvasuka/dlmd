6f2b81fbc64359ff2c36f280f94f3b3b2c9653d9
CACS-SONY\lucif <lucifernine86@gmail.com>
2018-08-28 23:31:34 -0700

    combine xyz decoders

--- dataset/xyz_parser.py
+++ dataset/xyz_parser.py
@@ -189,6 +189,84 @@ def _qm9_edges(g, e_representation='raw_distance'):
   return g, e
 
 
+##########################################
+def mol_graph_decoder(xyzfile):
+  with open(xyzfile, 'r') as f:
+    # Number of atoms
+    na = int(f.readline())
+
+    label = int(f.readline())
+    g = nx.Graph()
+
+    # Atom properties
+    atom_property = []
+    index_hash = {}
+    for i in range(na):
+      a_prop = f.readline()
+      a_prop = a_prop.replace('.*^', 'e')
+      a_prop = a_prop.replace('*^', 'e')
+      a_prop = a_prop.split()
+      atom_property.append(a_prop[:-1])
+      index_hash[a_prop[-1]] = i
+
+    # Add nodes
+    for i in range(na):
+      g.add_node(i,
+                 a_symbol=atom_property[i][0],
+                 coord=np.array(atom_property[i][1:4]).astype(np.float),
+                 pc=float(atom_property[i][4]))
+
+    # Add edges
+    for i in range(na):
+      e_prop = f.readline()
+      e_prop = e_prop.replace('.*^', 'e')
+      e_prop = e_prop.replace('*^', 'e')
+      e_prop = e_prop.split()
+
+      atom_i = index_hash[e_prop[1]]
+      num_neighbor = int(e_prop[2])
+      for j in range(num_neighbor):
+        try:
+          atom_j = index_hash[e_prop[2*j+3]]
+          g.add_edge(atom_i, atom_j,
+            bo=float(e_prop[2*j+4]),
+            distance=np.linalg.norm(g.node[atom_i]['coord'] - g.node[atom_j]['coord']))
+        except KeyError:
+          continue
+    
+    h = _mol_nodes(g)
+    g, e = _mol_edges(g)
+
+    return g, h, e, label
+
+def _mol_nodes(g):
+  h = []
+  for n, d in g.nodes(data=True):
+    h_t = []
+    h_t += [int(d['a_symbol'] == x) for x in ['Mo', 'S']]
+    h_t.append(d['pc'])
+    h.append(h_t)
+
+  return h
+
+def _mol_edges(g):
+  edge = {}
+  for n1, n2, d in g.edges(data=True):
+    e_t = []
+    e_t.append(d['bo'])
+    e_t.append(d['distance'])
+
+    edge[(n1, n2)] = e_t
+
+  g = nx.to_numpy_matrix(g)
+  e = np.zeros((g.shape[0], g.shape[1], len(list(edge.values())[0])))
+  for edg in edge.keys():
+    e[edg[0], edg[1], :] = edge[edg]
+    e[edg[1], edg[0], :] = edge[edg]
+
+  return g, e
+
+
 if __name__ == '__main__':
   import argparse
   
